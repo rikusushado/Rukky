3e53b52c75e50eac3018b6d4fcb1e44b91342e18
rikusushado <rikusushado@proton.me>
2026-09-15 17:16:12 +0200

    WIP: Core features

--- RuCore/RuCore.cmake
+++ RuCore/RuCore.cmake
@@ -1,9 +1,14 @@
-add_library(RuCore SHARED)
-
 set(RuCore_dir "${CMAKE_SOURCE_DIR}/RuCore")
+
+add_library(RuCore SHARED)
 
 include_directories("${RuCore_dir}/include")
 
+
+
 if (CMAKE_SYSTEM_NAME STREQUAL "Linux")
 	include("${RuCore_dir}/cmake/build_linux.cmake")
+else()
+	target_sources(RuCore PRIVATE "${RuCore_dir}/source/void.c")
+	message(FATAL_ERROR "Failed to build RuCore: Unsupported OS: ${CMAKE_SYSTEM_NAME}")
 endif()
--- RuTest/RuTest.cmake
+++ RuTest/RuTest.cmake
@@ -1,9 +1,14 @@
+set(RuTest_dir "${CMAKE_SOURCE_DIR}/RuTest")
+
 add_executable(RuTest_triangle)
 
 include_directories("${RuCore_dir}/include")
 
-set(RuTest_dir "${CMAKE_SOURCE_DIR}/RuTest")
 
 if (CMAKE_SYSTEM_NAME STREQUAL "Linux")
 	include("${RuTest_dir}/cmake/build_linux.cmake")
+
+else()
+	target_sources(RuCore PRIVATE "${RuTest_dir}/source/void.c")
+	message(FATAL_ERROR "Failed to build RuTest: Unsupported OS: ${CMAKE_SYSTEM_NAME}")
 endif()
